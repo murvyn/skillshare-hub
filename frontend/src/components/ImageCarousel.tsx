4cfff887ca72db631723006169313be7b2970e15
Murvyn <marvin.asamoah.123@gmail.com>
2025-01-29 19:47:05 +0000

    add poster images

--- frontend/src/components/ImageCarousel.tsx
+++ frontend/src/components/ImageCarousel.tsx
@@ -7,7 +7,20 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
-import { Card, CardContent } from "./ui/card";
+import poster1 from "@/assets/1.jpg";
+import poster2 from "@/assets/2.jpg";
+import poster3 from "@/assets/3.jpg";
+import poster4 from "@/assets/4.jpg";
+import poster5 from "@/assets/5.jpg";
+import Image from "next/image";
+
+const posters = [
+  { id: 1, image: poster1 },
+  { id: 2, image: poster2 },
+  { id: 3, image: poster3 },
+  { id: 4, image: poster4 },
+  { id: 5, image: poster5 },
+];
 
 const ImageCarousel = () => {
   const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
@@ -20,14 +33,12 @@ const ImageCarousel = () => {
         onMouseLeave={plugin.current.reset}
       >
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
+          {posters.map((poster) => (
+            <CarouselItem key={poster.id}>
               <div className="p-1">
-                <Card>
-                  <CardContent className="flex h-[30rem] items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
+                  <div className="flex h-[40rem] items-center justify-center ">
+                    <Image className="w-full h-full object-cover" src={poster.image} width={40000} height={2000} alt="posters" />
+                  </div>
               </div>
             </CarouselItem>
           ))}
